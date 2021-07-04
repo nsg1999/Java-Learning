c33839f641a50461ed0c9fbbc1b996c4e961e27c
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2021-07-04 14:02:16 +0530

    Create arrayCatchAndReset.java

---
+++ Assignment1/src/com/assignment/arrayCatchAndReset.java
@@ -0,0 +1,21 @@
+package com.assignment;
+
+public class arrayCatchAndReset {
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+    int indexPointer = 11;
+    
+		try {
+			arr[indexPointer] = 49;
+		}
+    
+		catch(ArrayIndexOutOfBoundsException excp) {
+			System.out.println("An exception has been caught, it says - \"" + excp.getMessage() + "\"");
+		}
+    
+		finally {
+			indexPointer = arr.length - 1;
+			System.out.println("Now, the indexPointer points to - " + indexPointer);
+		}
+	}
+}
