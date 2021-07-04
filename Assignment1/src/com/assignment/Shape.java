d8a9a50e6be5c3e0a89e58dcc415792661f491c6
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2021-07-04 13:35:49 +0530

    Create Shape.java
    Created the file Shape.java

---
+++ Assignment1/src/com/assignment/Shape.java
@@ -0,0 +1,31 @@
+package com.assignment;
+import java.util.Scanner;
+
+public class Shape {
+	private static int length, width;
+	
+	private boolean isSquare() {
+		return length == width;
+	}
+	
+	public static void main(String[] args) throws NotASquareException {
+		
+		Scanner sc = new Scanner(System.in);
+		Shape shapeObj = new Shape();
+		
+		System.out.println("Kindly enter the length and width: ");
+		
+		length = sc.nextInt();
+		width = sc.nextInt();
+		sc.close(); 
+	
+		if(shapeObj.isSquare()) 
+			System.out.println("\nIt is a square!");
+		else {
+			System.out.println("\nNote: an exception will be thrown now as the figure is not a square");
+			throw new NotASquareException("\nNot a square!");
+		}
+			
+	}
+	
+}
