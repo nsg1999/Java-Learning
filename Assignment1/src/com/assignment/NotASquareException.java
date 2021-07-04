93d08b0771bc17985dea6a77e85cda640a964439
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2021-07-04 13:40:14 +0530

    Updated NotASquareException.java

--- Assignment1/src/com/assignment/NotASquareException.java
+++ Assignment1/src/com/assignment/NotASquareException.java
@@ -1,7 +1,7 @@
 package com.assignment;
 
 public class NotASquareException extends Exception{
-	public NotASquareException(String err) {
-		super(err);
+	public NotASquareException(String errorMessage) {
+		super(errorMessage);
 	}
 }
