660ca304c6df3514cf132f937aba2ccff82b84ed
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2021-07-04 14:29:36 +0530

    Create exceptionWithTwoMethods.java

---
+++ Assignment1/src/com/assignment/exceptionWithTwoMethods.java
@@ -0,0 +1,47 @@
+package com.assignment;
+
+public class exceptionWithTwoMethods {
+	
+	String str;
+	
+	exceptionWithTwoMethods() {
+		this.str = null;
+	}
+	exceptionWithTwoMethods(String st) {
+		this.str = st;
+	}
+	
+	private void method1() throws customException {
+		try {
+			method2();
+		}
+		catch (NullPointerException e) {
+			throw new customException("String is null!");
+		}
+	}
+  
+	private void method2() {
+		System.out.println("\nThe string begins with - " + str[0]);
+	}
+  
+	public static void main(String[] args) {
+    String str = "Hello, I'm the author of this program!";
+    
+		exceptionWithTwoMethods excpObj1 = new exceptionWithTwoMethods();
+    exceptionWithTwoMethods excpObj2 = new exceptionWithTwoMethods(str);
+    
+		try {
+			excpObj1.method1();
+		}
+		catch(customException e) {
+			System.out.println("\nCustomized exception meesage: " + e.getMessage());
+		}		
+    
+    try {
+			excpObj2.method1();
+		}
+		catch(customException e) {
+			System.out.println("\nCustomized exception meesage: " + e.getMessage());
+		}	
+	}
+}
