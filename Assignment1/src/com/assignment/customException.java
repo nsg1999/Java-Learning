2851e4067cff4cb339dbed571f4a44415a2dff58
Neelam Gurjar <39006317+nsg1999@users.noreply.github.com>
2021-07-04 14:49:30 +0530

    Update customException.java

--- Assignment1/src/com/assignment/customException.java
+++ Assignment1/src/com/assignment/customException.java
@@ -1,8 +1,7 @@
 package com.assignment;
 
-public class customException extends NullPointerException{
+public class customException extends NullPointerException {
 	public customException(String errorMessage) {
-		System.out.println("Custom Exception: "+ errorMessage);
+		System.out.println("\nCustom Exception: " + errorMessage);
 	}
-
 }
